71f848e336df8f6887306e0f966f7c61bde73ca8
Narmin <mnermin064@gmail.com>
2023-11-07 21:48:23 +0400

    React enter

---
+++ React/lesson1/src/App.js
@@ -0,0 +1,21 @@
+import Box from "./components/Box";
+
+function App() {
+  return (
+    <>
+      <div className="App">
+        <h1 style={{ color: "blue", fontWeight: "bold" }}>Salam</h1>
+        <p>Hello everyone</p>
+        <Box />
+        <Box />
+        <Box />
+        <Box />
+      </div>
+    </>
+  );
+}
+
+export default App;
+
+// JSX - Javascript Xml
+// SPA - Single Page Application
---
+++ React/lesson1/src/components/Box.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Box = () => {
+  return (
+    <div>Box 1</div>
+  )
+}
+
+export default Box
